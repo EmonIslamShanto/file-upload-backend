2c532415fb83e11c3b3b7f7b77b243b63c1bd000
Emon Islam Shanto <98974290+EmonIslamShanto@users.noreply.github.com>
2024-11-08 00:56:36 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const cors = require("cors");
 const uploadRoutes = require('./routes/uploadRoute');
+const corsOptions = {
+  origin: 'https://task-dashboard-alpha.vercel.app/',  // Allow only this origin
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
 
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 const PORT = 5252;
 
 app.use(express.json());
